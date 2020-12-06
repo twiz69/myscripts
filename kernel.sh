41fa197673f4e257a0c8340303f1372e5c3d4437
Reinazhard <muh.alfarozy@gmail.com>
2020-12-07 02:22:44 +0700

    fix build not uploaded
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -140,6 +140,7 @@ exports() {
 	export KBUILD_BUILD_USER="reina"
 	export ARCH=arm64
 	export SUBARCH=arm64
+	export token=$TELEGRAM_TOKEN
 
 		KBUILD_COMPILER_STRING=$("$TC_DIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')
 		PATH=$TC_DIR/bin/:$PATH
