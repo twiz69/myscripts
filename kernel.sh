e2705fa971f8c58df1fd9c1cbf751f6ffe0566d7
Reinazhard <muh.alfarozy@gmail.com>
2021-02-25 02:20:05 +0700

    use GCC 10
    Signed-off-by: Reinazhard <muh.alfarozy@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -125,8 +125,8 @@ COMMIT_HEAD=$(git log --oneline -1)
 
  clone() {
 	echo " "
-		msg "|| Cloning GCC 9.3.0 baremetal ||"
-		git clone --depth=1 https://github.com/arter97/arm64-gcc gcc64
+		msg "|| Cloning GCC 10 baremetal ||"
+		git clone --depth=1 https://github.com/silont-project/aarch64-elf-gcc -b arm64/10 gcc64
 		git clone --depth=1 https://github.com/arter97/arm32-gcc gcc32
 		GCC64_DIR=$KERNEL_DIR/gcc64
 		GCC32_DIR=$KERNEL_DIR/gcc32
